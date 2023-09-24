b829113ae71d80810770741eaff8219e3a9926ef
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-09-24 17:29:35 -0300

    http

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,13 +1,18 @@
 package med.voll.api;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		new SpringApplicationBuilder()
+				.sources(ApiApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
 	}
 
 }
--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,25 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("medicos")
+@RequestMapping("/doctor")
 public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
 
-    @PostMapping
+    @PostMapping("/search")
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
-    @GetMapping
+    @GetMapping("/status")
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> findAll(@PageableDefault(size = 10, sort = {"nome"}) Pageable page){
+        return repository.findAll(page).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -35,10 +41,17 @@ public class MedicoController {
     }
 
     @DeleteMapping("/{id}")
-    @Transactional
-    public void excluir(@PathVariable Long id) {
-        var medico = repository.getReferenceById(id);
-        medico.excluir();
+    @jakarta.transaction.Transactional
+    public void delete(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+    @DeleteMapping("/logical/{id}")
+    @jakarta.transaction.Transactional
+    public ResponseEntity logicalDelete(@PathVariable Long id) {
+        var doctor = repository.getReferenceById(id);
+        doctor.excluir();
+        return ResponseEntity.noContent().build();
     }
 
 
