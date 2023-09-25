ec85399d0d89b2c47f90f789c828ff210dcda79a
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-09-24 23:09:34 -0300

    Auth

---
+++ src/main/resources/db/migration/V7__insert-values-table-users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users VALUES(1,"ana.lais", "$2b$12$gj5W0sJ9ZRmUgYHpR/FWYOOhC686BejsvI8/AWPj94e7rZ.rNiZAO"); -- password: 123456
+INSERT INTO users VALUES(2,"fe", "$2b$12$dogBKepo/lnW864cG1On8.q/1Qhk2leVNkKEBGZzdSJ4NDQx9g6tW"); -- password: fe
