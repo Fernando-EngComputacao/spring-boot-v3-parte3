6be2b1bcd8fd881b9d075a6bada55b1b1671c544
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-09-24 22:35:55 -0300

    insert into doctors

---
+++ src/main/resources/db/migration/V6__insert-values-table-doctors.sql
@@ -0,0 +1,39 @@
+-- Inserção 1
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("João da Silva", "joao.silva@gmail.com", "123456", "GINECOLOGIA", "62 9 9567-3049", "Rua b", "Setor ABC", "75381234", "Goiânia", "GO", "10", "apto 301", 1);
+
+-- Inserção 2
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Maria Santos", "maria.santos@gmail.com", "789012", "ORTOPEDIA", "62 9 9567-3049", "Avenida X", "Setor XYZ", "75382456", "Anápolis", "GO", "20", "sala 102", 1);
+
+-- Inserção 3
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("José Oliveira", "jose.oliveira@gmail.com", "654321", "DERMATOLOGIA", "62 9 9567-3049", "Rua Y", "Setor UVW", "75383679", "Goiânia", "GO", "15", "casa", 1);
+
+-- Inserção 4
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Ana Rodrigues", "ana.rodrigues@gmail.com", "987654", "CARDIOLOGIA", "62 9 9567-3049", "Avenida Z", "Setor LMN", "75384890", "Goiânia", "GO", "25", "apto 502", 1);
+
+-- Inserção 5
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Carlos Ribeiro", "carlos.ribeiro@gmail.com", "234567", "GINECOLOGIA", "62 9 9567-3049","Rua C", "Setor OPQ", "75386123", "Anápolis", "GO", "12", "casa", 1);
+
+-- Inserção 6
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Lúcia Fernandes", "lucia.fernandes@gmail.com", "876543", "ORTOPEDIA", "62 9 9567-3049", "Avenida A", "Setor RST", "75387345", "Goiânia", "GO", "18", "apto 205", 1);
+
+-- Inserção 7
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Ricardo Castro", "ricardo.castro@gmail.com", "345678", "DERMATOLOGIA", "62 9 9567-3049", "Rua B", "Setor UVW", "75388567", "Goiânia", "GO", "8", "sala 201", 1);
+
+-- Inserção 8
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Patrícia Alves", "patricia.alves@gmail.com", "567890", "CARDIOLOGIA", "62 9 9567-3049", "Avenida D", "Setor XYZ", "75389789", "Anápolis", "GO", "30", "casa", 1);
+
+-- Inserção 9
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Fernanda Lima", "fernanda.lima@gmail.com", "432109", "GINECOLOGIA", "62 9 9567-3049", "Rua E", "Setor OPQ", "75381012", "Goiânia", "GO", "14", "apto 401", 1);
+
+-- Inserção 10
+INSERT INTO doctors (name, email, crm, specialty, telephone, place, neighborhood, cep, city, uf, number, complement, active)
+VALUES ("Paulo Nunes", "paulo.nunes@gmail.com", "765432", "ORTOPEDIA", "62 9 9567-3049",  "Avenida F", "Setor LMN", "75381123", "Goiânia", "GO", "22", "sala 304", 1);
