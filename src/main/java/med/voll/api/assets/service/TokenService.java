ba9424a1d03c3b05984ddde0d2ded8a82915c5a5
Fernando Furtado <fernando.furtadocarrilho@gmail.com>
2023-10-02 14:07:57 -0300

    Using the JWT token to make HTTP requests

--- src/main/java/med/voll/api/assets/service/TokenService.java
+++ src/main/java/med/voll/api/assets/service/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.assets.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.extern.slf4j.Slf4j;
 import med.voll.api.domain.user.User;
 import med.voll.api.infra.config.ValuesConfig;
@@ -23,15 +24,31 @@ public class TokenService {
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(values.getUrlSecret());
-            return JWT.create()
+            var token =  JWT.create()
                     .withIssuer("API DOCTOR")
                     .withSubject(user.getUsername())
                     .withExpiresAt(expiresDate())
                     .sign(algorithm);
 //                    .withClaim("id", user.getId())
 //                    .withClaim("password", user.getPassword())
+            log.info("Generating token {}", token);
+            return token;
         } catch (JWTCreationException e) {
-            throw new RuntimeException("Error generating token : " + e.getMessage(), e);
+            throw new RuntimeException("***Error generating token : " + e.getMessage(), e);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(values.getUrlSecret());
+            return JWT.require(algorithm)
+                    .withIssuer("API DOCTOR")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("***Invalid or expired JWT token!");
         }
     }
 
